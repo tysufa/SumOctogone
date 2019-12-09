acece8f24b97b3406138ec1666bc0992eb34c0b7
tysufa <58104576+tysufa@users.noreply.github.com>
2019-12-09 08:54:09 +0100

    Update principal.py

--- sumo_fighters/principal.py
+++ sumo_fighters/principal.py
@@ -12,11 +12,14 @@ pygame.display.set_caption("SumOctogone")
 
 # affichage de l'icone
 
+
+# creation d'un timer
+timer = pygame.time.Clock()
+
 # chargement des différentes images du jeu
 img_background = pygame.image.load("images/background menu.png").convert()
 img_sumo_player = pygame.image.load("images/white.png").convert_alpha()
 img_ile = pygame.image.load("images/ile.png").convert_alpha()
-
 
 # creation d'un rect pour la position du sumo
 position_perso = img_sumo_player.get_rect(center=(270, 300))
@@ -25,9 +28,12 @@ position_perso = img_sumo_player.get_rect(center=(270, 300))
 pygame.display.flip()
 
 # assignement des variables
+x, y = 270, 300
 vx = 0  # vitesse x
 vy = 0  # vitesse y
 continuer = 1
+gravite = 2.5
+
 
 # creation de la boucle pour laisser la fenetre ouverte
 while continuer:
@@ -35,23 +41,27 @@ while continuer:
         if evenement.type == QUIT:
             continuer = 0
 
-        elif evenement.type == KEYDOWN:
-            if evenement.key == K_RIGHT:
-                vx += 1
-            if evenement.key == K_LEFT:
-                vx -= 1
+        if evenement.type == KEYDOWN:
+            if evenement.key == K_UP:
+                vy = -30
 
-        elif evenement.type == KEYUP:
-            if evenement.key == K_RIGHT:
-                vx -= 1
-            if evenement.key == K_LEFT:
-                vx += 1
+    timer.tick(60)
+    # creation de la variable keypress
+    keypress = pygame.key.get_pressed()
 
-    position_perso.left += vx
+    vy += gravite
+
+    vx = (keypress[K_RIGHT] - keypress[K_LEFT]) * 3
+
+    x += vx
+    y += vy
+
+    y = min(330, y)
 
     # affichage des images
     window.blit(img_background, (0, 0))
     window.blit(img_ile, (longueur / 2 - 500 / 2, hauteur / 2 - 30))
-    window.blit(img_sumo_player, position_perso)
+    window.blit(img_sumo_player, (x, y))
 
     pygame.display.flip()
+
