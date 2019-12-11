1039895b49f112576fe327133c8c4f20475f3492
tysufa <58104576+tysufa@users.noreply.github.com>
2019-12-11 09:53:14 +0100

    Update principal.py

--- sumo_fighters/jeux/version_spam/principal.py
+++ sumo_fighters/jeux/version_spam/principal.py
@@ -1,5 +1,6 @@
 from pygame.locals import *
-from jeux.version_classique.constantes import *
+from constantes import *
+import time
 
 pygame.init()
 
@@ -16,7 +17,7 @@ pygame.display.set_icon(img_icone)
 # creation d'un timer
 timer = pygame.time.Clock()
 
-# chargement des différentes images du jeu
+# chargement des diffÃ©rentes images du jeu
 img_background = pygame.image.load("images/background menu.png").convert()
 img_sumo_player = pygame.image.load("images/white.png").convert_alpha()
 img_sumo_player2 = pygame.image.load("images/green.png").convert_alpha()
@@ -41,12 +42,12 @@ x_perso1, y_perso1, x_perso2, y_perso2 = 545, 300, 210, 300  # declaration des v
 
 vx_perso1 = vy_perso1 = vx_perso2 = vy_perso2 = 0  # vitesse x et y des joueurs
 
-nb_saut_sumo1 = nb_saut_sumo2 = 0
-
 vie_sumo1 = vie_sumo2 = 100
 
 continuer = 1
 gravite = 2.5
+
+temps = time.time()
 
 # creation de la boucle principal
 while continuer:
@@ -56,60 +57,64 @@ while continuer:
 
         if evenement.type == KEYDOWN:
 
-            if nb_saut_sumo1 < 2:  # detecte si le sumo à fait 2 sauts ou pas
+
+            if y_perso1 == 330:
                 if evenement.key == K_UP:
                     vy_perso1 = -25  # hauteur du saut
-                    nb_saut_sumo1 += 1
-            elif nb_saut_sumo1 >= 2:
-                nb_saut_sumo1 = 0
 
-            if nb_saut_sumo2 < 2:  # permet de faire le saut aux sumos
+            if y_perso2 == 330:
                 if evenement.key == K_w:
                     vy_perso2 = -25  # hauteur du saut
-                    nb_saut_sumo2 += 1
-            elif nb_saut_sumo2 >= 2:
-                nb_saut_sumo2 = 0
 
             if evenement.key == K_SPACE:
                 print("y1 : ", y_perso1, "x1", x_perso1, " vie 1", vie_sumo1, "\ny2 : ", y_perso2, "x2", x_perso2,
                       "vie 2", vie_sumo2)
 
+            if evenement.key == K_m:
+                if sumo_rect1.colliderect(sumo_rect2):
+                    vie_sumo1 -= 1
+                    print(vie_sumo1)
+                    if vie_sumo1 <= 0:
+                        continuer = 0
+
     timer.tick(60)
     # creation de la variable keypress
     keypress = pygame.key.get_pressed()
 
-    vy_perso1 += gravite  # ajout de la gravité pour ralentir le saut des personnages et leur permettre de retomber
+    vy_perso1 += gravite  # ajout de la gravitÃ© pour ralentir le saut des personnages et leur permettre de retomber
     min(20, vy_perso1)
 
     vy_perso2 += gravite
     min(20, vy_perso2)
 
     vx_perso1 = (keypress[K_RIGHT] - keypress[
-        K_LEFT]) * 5  # se sont des booléans qui valent 1 quand une touche est préssé
+        K_LEFT]) * 5  # se sont des boolÃ©ans qui valent 1 quand une touche est prÃ©ssÃ©
+
+
 
     vx_perso2 = (keypress[K_d] - keypress[K_a]) * 5
+
+    """
     if keypress[K_e]:
         if keypress[K_RCTRL]:
             print("SPAAAAAAAAAAAM")
-    if keypress[K_e]:  # permet de détécter si les sumos tapent
+    if keypress[K_e]:  # permet de dÃ©tÃ©cter si les sumos tapent
         if sumo_rect2.colliderect(
-                sumo_rect1):  # si les sumos tapent et qu'ils se touchent en même temps alors le if est vérifié
+                sumo_rect1):  # si les sumos tapent et qu'ils se touchent en mÃªme temps alors le if est vÃ©rifiÃ©
             vie_sumo1 -= 1
             if vie_sumo1 <= 0:
                 print("mort sumo 1")
 
-    if keypress[K_RCTRL]:  # permet de détécter si les sumos tapent
+    if keypress[K_RCTRL]:  # permet de dÃ©tÃ©cter si les sumos tapent
         if sumo_rect1.colliderect(
-                sumo_rect2):  # si les sumos tapent et qu'ils se touchent en même temps alors le if est vérifié
+                sumo_rect2):  # si les sumos tapent et qu'ils se touchent en mÃªme temps alors le if est vÃ©rifiÃ©
             vie_sumo2 -= 1
             if vie_sumo2 <= 0:
                 print("mort sumo 2")
 
-    if keypress[K_SPACE]:
-        if sumo_rect1.colliderect(sumo_rect2):
-            continuer = 0
+    """
 
-    x_perso1 += vx_perso1  # actualisation de la position du personnage par rapport à sa vitesse
+    x_perso1 += vx_perso1  # actualisation de la position du personnage par rapport Ã  sa vitesse
     y_perso1 += vy_perso1
 
     x_perso2 += vx_perso2
@@ -128,10 +133,10 @@ while continuer:
     else:
         pass
 
-    if y_perso1 >= 600 or y_perso2 >= 600:  # ferme le jeu quand l'un des sumos tombe de l'écran
+    if y_perso1 >= 600 or y_perso2 >= 600:  # ferme le jeu quand l'un des sumos tombe de l'Ã©cran
         continuer = 0
 
-    sumo_rect1.topleft = (x_perso1, y_perso1)  # récupération de la position des rects pour les collisions
+    sumo_rect1.topleft = (x_perso1, y_perso1)  # rÃ©cupÃ©ration de la position des rects pour les collisions
     sumo_rect2.topleft = (x_perso2, y_perso2)
 
     rect_ile_pricipale.topleft = (longueur / 2 - 500 / 2, hauteur / 2 - 30)
